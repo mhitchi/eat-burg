5423e4265a9e7d18453eb5a5e9fe5e50c22d9135
mhitchi <mayhitchings@gmail.com>
2020-02-11 17:14:39 -0500

    controller initial files

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const burgerJS = require("../models/burger");
+
+const app = express();
+
+//create router (https://expressjs.com/en/guide/routing.html)
---
+++ models/burger.js
@@ -0,0 +1,7 @@
+import { sequelize } from "../../../inclass/02/02_11/sequelize_demo/models";
+
+sequelize.sync();
+
+module.exports = () => {
+
+}
