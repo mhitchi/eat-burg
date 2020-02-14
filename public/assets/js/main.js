5e6e65c5450afdae94ef83d73ea3fbba95139096
mhitchi <mayhitchings@gmail.com>
2020-02-13 21:10:56 -0500

    getting devour btn by id

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,18 @@
 //ajax call
+
+//post request for new burger
+
+//put request for eaten burger
+
+console.log("getting jquery");
+
+//get elements from DOM
+const $devourBtn = $(".devour");
+
+
+$devourBtn.on('click', function() {
+  console.log("clicking devour");
+  // TRIED const id = $(this).data("id");
+  const id = this.id
+  console.log(id);
+});
